fc7e8bbb4f3b5dea0bf49e397df642be94186189
Nguyễn Nhật Tiến <bryantien2405@gmail.com>
2026-06-28 23:43:29 +0700

    Fix

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def get_valid_moves(state):
     moves = []
     blank_index = state.index(0)
@@ -29,36 +28,4 @@ def apply_move_ucs(state, move_name):
     
     move_cost = state_list[new_index]  
     state_list[blank_index], state_list[new_index] = state_list[new_index], state_list[blank_index]
-=======
-def get_valid_moves(state):
-    moves = []
-    blank_index = state.index(0)
-    if blank_index > 2: moves.append('Up')
-    if blank_index < 6: moves.append('Down')
-    if blank_index % 3 != 0: moves.append('Left')
-    if (blank_index + 1) % 3 != 0: moves.append('Right')
-    return moves
-
-def apply_move(state, move_name):
-    state_list = list(state)
-    blank_index = state_list.index(0)
-    if move_name == 'Up': new_index = blank_index - 3
-    elif move_name == 'Down': new_index = blank_index + 3
-    elif move_name == 'Left': new_index = blank_index - 1
-    elif move_name == 'Right': new_index = blank_index + 1
-    
-    state_list[blank_index], state_list[new_index] = state_list[new_index], state_list[blank_index]
-    return tuple(state_list)
-
-def apply_move_ucs(state, move_name):
-    state_list = list(state)
-    blank_index = state_list.index(0)
-    if move_name == 'Up': new_index = blank_index - 3
-    elif move_name == 'Down': new_index = blank_index + 3
-    elif move_name == 'Left': new_index = blank_index - 1
-    elif move_name == 'Right': new_index = blank_index + 1
-    
-    move_cost = state_list[new_index]  
-    state_list[blank_index], state_list[new_index] = state_list[new_index], state_list[blank_index]
->>>>>>> 72738f666174b033596d190f1bd45171d50458fe
     return tuple(state_list), move_cost
